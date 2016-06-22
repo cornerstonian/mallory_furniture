af883be20bfca8da2f9852d1962c84cb6a545703
cornerstonian <vois.cornerstone@gmail.com>
2016-06-21 21:26:03 -0500

    improved styling and fixed views and paths/links

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,32 +12,32 @@ class ProductsController < ApplicationController
     @product = @products.find {|p| p.pid == params[:id]}
   end
 
-  def fetch_products
-    products = []
+          def fetch_products
+            products = []
 
-    CSV.foreach("#{Rails.root}/mf_inventory.csv", headers: true) do |row|
-      product_hash = row.to_hash
+            CSV.foreach("#{Rails.root}/mf_inventory.csv", headers: true) do |row|
+              product_hash = row.to_hash
 
-      if product_hash["quantity"].to_i > 0
+                if product_hash["quantity"].to_i > 0
 
-      product = Product.new
-      product.pid = product_hash['pid']
-      product.item = product_hash['item']
-      product.description = product_hash['description']
-      product.price = product_hash['price']
-      product.condition = product_hash['condition']
-      product.dimension_w = product_hash['dimension_w']
-      product.dimension_l = product_hash['dimension_l']
-      product.dimension_h = product_hash['dimension_h']
-      product.img_file = product_hash['img_file']
-      product.quantity = product_hash['quantity']
-      product.category = product_hash['category']
+                product = Product.new
+                product.pid = product_hash['pid']
+                product.item = product_hash['item']
+                product.description = product_hash['description']
+                product.price = product_hash['price']
+                product.condition = product_hash['condition']
+                product.dimension_w = product_hash['dimension_w']
+                product.dimension_l = product_hash['dimension_l']
+                product.dimension_h = product_hash['dimension_h']
+                product.img_file = product_hash['img_file']
+                product.quantity = product_hash['quantity']
+                product.category = product_hash['category']
 
-      products << product
-     end
-    end
-    products
-  end
+                products << product
+               end
+            end
+            products
+          end
 
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,13 @@
 class Product
     attr_accessor :pid, :item, :description, :price, :condition, :dimension_w, :dimension_l, :dimension_h, :img_file, :quantity, :category
-      #method to calculate discount
 
-      def self.discount_price
-      if @product.condition == "good"
-        (@product.price.to_i * 0.9)
-      elsif @product.condition == "average"
-        @product.price.to_i * 0.8
-      else
-      @product.price
+      def discount_price
+         if @product.condition == "good"
+          (@product.price.to_i * 0.9)
+         elsif @product.condition == "average"
+          @product.price.to_i * 0.8
+         else
+           @product.price
       end
 
 end

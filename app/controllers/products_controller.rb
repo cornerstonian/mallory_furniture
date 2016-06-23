6e78815c9764052620f72c2d4c225aef2a462513
cornerstonian <vois.cornerstone@gmail.com>
2016-06-23 15:34:21 -0500

    styling improved, still not able to call discount method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,32 +12,45 @@ class ProductsController < ApplicationController
     @product = @products.find {|p| p.pid == params[:id]}
   end
 
-          def fetch_products
-            products = []
+  def discount_price
+    case @product.condition
+    when @product.condition == "good"
+      "#{ @price *0.9 } ITEM ON CLEARANCE! 10% off!"
+    when @prodcut.condition == "average"
+      "#{ @price *0.8 } ITEM ON CLEARANCE! 20% off!"
+    else
+      @price
+    end
+  end
 
-            CSV.foreach("#{Rails.root}/mf_inventory.csv", headers: true) do |row|
-              product_hash = row.to_hash
 
-                if product_hash["quantity"].to_i > 0
 
-                product = Product.new
-                product.pid = product_hash['pid']
-                product.item = product_hash['item']
-                product.description = product_hash['description']
-                product.price = product_hash['price']
-                product.condition = product_hash['condition']
-                product.dimension_w = product_hash['dimension_w']
-                product.dimension_l = product_hash['dimension_l']
-                product.dimension_h = product_hash['dimension_h']
-                product.img_file = product_hash['img_file']
-                product.quantity = product_hash['quantity']
-                product.category = product_hash['category']
+    def fetch_products
+      products = []
 
-                products << product
-               end
-            end
-            products
-          end
+      CSV.foreach("#{Rails.root}/mf_inventory.csv", headers: true) do |row|
+        product_hash = row.to_hash
+
+          if product_hash["quantity"].to_i > 0
+
+          product = Product.new
+          product.pid = product_hash['pid']
+          product.item = product_hash['item']
+          product.description = product_hash['description']
+          product.price = product_hash['price']
+          product.condition = product_hash['condition']
+          product.dimension_w = product_hash['dimension_w']
+          product.dimension_l = product_hash['dimension_l']
+          product.dimension_h = product_hash['dimension_h']
+          product.img_file = product_hash['img_file']
+          product.quantity = product_hash['quantity']
+          product.category = product_hash['category']
+
+          products << product
+         end
+      end
+      products
+    end
 
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,4 @@
 class Product
     attr_accessor :pid, :item, :description, :price, :condition, :dimension_w, :dimension_l, :dimension_h, :img_file, :quantity, :category
-
-      def discount_price
-         if @product.condition == "good"
-          (@product.price.to_i * 0.9)
-         elsif @product.condition == "average"
-          @product.price.to_i * 0.8
-         else
-           @product.price
-      end
-
+    
 end
